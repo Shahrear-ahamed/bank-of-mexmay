f89d19e2035a778e0b9bb6f55002bcf260f10100
Shahrear-ahamed <dev.shahrear@gmail.com>
2022-02-10 20:02:15 +0600

    basic calculation are added

--- js/just.js
+++ js/just.js
@@ -13,14 +13,48 @@ const withdrowBtn = document.getElementById("withdrow_btn");
 const withdrow = document.getElementById("withdrow_amount");
 
 depositBtn.addEventListener("click", function(){
-    const amounts = parseFloat(deposit.value);
-    if(typeof amounts == "string"){
-        alert("Please Input number only");
-    }else if( amounts < 0){
-        alert("Money can not deposit negetive number");
-    }else if( amounts == 0){
-        alert("You can not deposit 0 ");
-    }
-    totalDeposit.innerText = amounts;
+    const depositAmounts = parseFloat(deposit.value);
+    const previousDeposit = parseFloat(totalDeposit.innerText);
+    const totalBalanceNumber = parseFloat(totalBalance.innerText);
+
+    const lastAmountDeposit = previousDeposit + depositAmounts;
+
+    // add total balance 
+    const finalBalance = depositAmounts + totalBalanceNumber;
+    
+    totalDeposit.innerText = lastAmountDeposit;
     deposit.value = "";
+
+    totalBalance.innerText = finalBalance;
 })
+
+// withdrow section functior
+withdrowBtn.addEventListener("click", function(){
+    const withdrowAmounts = parseFloat(withdrow.value);
+    const previousWithdrow = parseFloat(totalWithdrow.innerText);
+    const totalBalanceNumber = parseFloat(totalBalance.innerText);
+
+    const lastAmountWithdrow = withdrowAmounts + previousWithdrow;
+
+    //total cost taka in total balance
+    const totalLastbalance = totalBalanceNumber - withdrowAmounts;
+
+    totalWithdrow.innerText = lastAmountWithdrow;
+    withdrow.value = "";
+
+    totalBalance.innerText = totalLastbalance;
+})
+
+
+// error handeling 
+// if(typeof depositAmounts == "string"){
+//     alert("Please Input number only");
+//     return
+// }
+// if( depositAmounts < 0){
+//     alert("Money can not deposit negetive number");
+//     return
+// }else if( depositAmounts == 0){
+//     alert("You can not deposit 0 ");
+//     return
+// }
