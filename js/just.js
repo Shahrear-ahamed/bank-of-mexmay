9777d37eb281dace10074d81226cc5308fcc62a4
Shahrear-ahamed <dev.shahrear@gmail.com>
2022-02-11 19:52:04 +0600

    short some code to function and make it easy

--- js/just.js
+++ js/just.js
@@ -1,26 +1,30 @@
-document.getElementById("deposit_btn").addEventListener("click", function(){
-    const inputDepositText = document.getElementById("deposit_amount");
+function inputAmount(inputId){
+    const inputDepositText = document.getElementById(inputId);
     const inputDeposit = parseFloat(inputDepositText.value);
 
-    const depositText = document.getElementById("total_deposit");
-    const depositAmount = parseFloat(depositText.innerText);
-    depositText.innerText = depositAmount + inputDeposit;
     inputDepositText.value = "";
+    return inputDeposit;
+}
+
+function balanceAmount(balanceAmount, depositBalance){
+    const depositText = document.getElementById(balanceAmount);
+    const depositAmount = parseFloat(depositText.innerText);
+    depositText.innerText = depositAmount + depositBalance;
+}
+
+document.getElementById("deposit_btn").addEventListener("click", function(){
+    const inputDeposit = inputAmount("deposit_amount");
+    balanceAmount("total_deposit", inputDeposit);
 
     const totalDepositText = document.getElementById("total_balance");
     const totalDeposit = parseFloat(totalDepositText.innerText);
+
     totalDepositText.innerText = totalDeposit + inputDeposit;
 })
 
 document.getElementById("withdrow_btn").addEventListener("click", function(){
-    const inputWithdrowText = document.getElementById("withdrow_amount");
-    const inputWithdrow = parseFloat(inputWithdrowText.value);
-
-    const withdrowText = document.getElementById("total_withdrow");
-    const withdrow = parseFloat(withdrowText.innerText);
-
-    withdrowText.innerText = withdrow + inputWithdrow;
-    inputWithdrowText.value = "";
+    const inputWithdrow = inputAmount("withdrow_amount");
+    balanceAmount("total_withdrow", inputWithdrow);
 
     const totalWithdrowText = document.getElementById("total_balance");
     const totalWithdrow = parseFloat(totalWithdrowText.innerText);
